78a4adda426f15bc0e1125aef837e197721d7c3a
yashsharma127 <yashshar.2323@gmail.com>
2023-06-18 23:56:45 +0530

    Final unicef code

--- Part-1/unicafe/src/App.js
+++ Part-1/unicafe/src/App.js
@@ -1,22 +1,52 @@
 import { useState } from 'react'
 
+const Headings = (props) => <h1>{props.text}</h1>
 
-const goodCounter = (good,setGood) => {
-  setGood(good + 1)
+const Button = ({handleClick,text}) => <button onClick={handleClick} > {text} </button>
+  
+const Statistics = (props) => {
+  if(props.good+props.neutral+props.bad===0 ){
+  return(
+    <div>
+      <h1>{props.text}</h1>
+      No Feedback
+    </div>
+  )
+}
+  return(
+    <div>
+      <h1>{props.text}</h1>
+      <table>
+          <tbody>
+              <StatisticLine text = "good"     value ={props.good} />
+              <StatisticLine text = "neutral"  value ={props.neutral} />
+              <StatisticLine text = "bad"      value ={props.bad} />
+              <StatisticLine text = "all"      value = {props.good+props.neutral+props.bad}/>
+              <StatisticLine text = "Average"  value = {(props.good-props.bad)/(props.good+props.neutral+props.bad)} />
+              <StatisticLine text = "Positive" value = {((props.good)/(props.good+props.neutral+props.bad))*100+'%'}/>
+          </tbody>
+      </table>
+    </div>
+  )
+  
 }
 
-const neutralCounter = (neutral,setNeutral) => {
-  setNeutral(neutral + 1)
-}
+const goodCounter = (good,setGood) => setGood(good + 1)
+ 
+const neutralCounter = (neutral,setNeutral) => setNeutral(neutral + 1)
 
-const badCounter = (bad,setBad) => {
-  setBad(bad + 1)
-}
+const badCounter = (bad,setBad) => setBad(bad + 1)
 
+const StatisticLine = (props) => 
+        <tr>
+          <td>{props.text}</td>
+          <td>{props.value}</td>
+        </tr>
+  
 
 
 const App = () => {
-  // save clicks of each button to its own state
+  const text = "Give Feedback"
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -25,21 +55,19 @@ console.log("good button clicked",good,"times")
 console.log("neutral button clicked",neutral,"times")
 console.log("bad button clicked",bad,"times")
 
- 
   return (
     <div>
-      <h1>Give Feedback</h1>
-      
-      <button onClick={() => goodCounter(good , setGood)}>good</button>
-      <button onClick={() => neutralCounter(neutral,setNeutral)}>neutral</button>
-      <button onClick={() => badCounter(bad,setBad)}>bad</button>
+      <Headings text = {text}/>
 
-      <h1>Statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <Button text= {"good"} handleClick = { () => goodCounter(good,setGood) }/>
+      <Button text= {"neutral"} handleClick = { () => neutralCounter(neutral,setNeutral)}/>
+      <Button text= {"bad"} handleClick = { () => badCounter(bad,setBad)}/>
+
+      <Statistics text = {"Statistics"} 
+                  good = {good} neutral = {neutral} bad = {bad}/>
     </div>
   )
 }
+
 
 export default App
