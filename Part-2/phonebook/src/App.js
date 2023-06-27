96038ec229a9174ec0f4711f4dc34121c5bd2918
yashsharma127 <yashshar.2323@gmail.com>
2023-06-27 21:38:00 +0530

    part-2 exercise-2.11 updates i.e. adding local db server

--- Part-2/phonebook/src/App.js
+++ Part-2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Filter = ({filterPerson,handelFilterperson,filterPersons}) => {
     return(
@@ -63,17 +64,24 @@ const Persons = ({personsToShow}) => {
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [showAll, setShowAll] = useState(true) 
   const [filterPerson , setFilterperson] = useState('')
   const [result, setResult] = useState([]);
+
+  useEffect(() => {
+    console.log('effect code')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fullfiled')
+        setPersons(response.data)
+      })
+  },[])
+  console.log('render', persons.length, 'persons')
+
 
   const personsToShow = showAll
     ? persons
