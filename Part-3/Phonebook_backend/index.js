75b4163a026e6d2ea4b53b589b2ac419d2b76a02
yashsharma127 <yashshar.2323@gmail.com>
2023-07-16 23:57:29 +0530

    adding the delete functionality to phonebookbackend fuly resposive to frontend and database according to exersice 3.17

--- Part-3/Phonebook_backend/index.js
+++ Part-3/Phonebook_backend/index.js
@@ -100,6 +100,22 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+  
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+  
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+      .catch(error => next(error))
+  })
+
 
 app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndRemove(request.params.id)
