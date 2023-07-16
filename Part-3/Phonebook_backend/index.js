338ec6d73b39221963496b7eb5ab4e2a56482c97
yashsharma127 <yashshar.2323@gmail.com>
2023-07-16 23:56:08 +0530

    added delete and error handling to the phonebookbackend according to exersice 3.15 and 3.16

--- Part-3/Phonebook_backend/index.js
+++ Part-3/Phonebook_backend/index.js
@@ -11,7 +11,34 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+  
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+ 
+  //Error handling middleware
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+}
+  
+
 let persons = []
+
+app.use(requestLogger)
 
 morgan.token('req-body', (req) => {
     if(req.method === 'POST') {
@@ -34,16 +61,20 @@ app.get('/info', (request, response) => {
     `)
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
 
-    if(person){
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
-})
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+      .then(note => {
+        if (note) {
+          response.json(note)
+        } else {
+          response.status(404).end()
+        }
+      })
+  
+      .catch(error => next(error))
+  })
+
 
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
@@ -70,13 +101,16 @@ app.post('/api/persons', (request, response) => {
 })
 
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id =Number(request.params.id)
-    persons =persons.filter(person => person.id !== id)
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndRemove(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
+  })
 
-    response.status(204).end()
-})
-
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
