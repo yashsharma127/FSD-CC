ca0e9372a48cd5f8fa46111515379fbe0037cd32
yashsharma127 <yashshar.2323@gmail.com>
2023-07-18 23:55:13 +0530

    added exersice 3.19 updates

--- Part-3/Phonebook_backend/index.js
+++ Part-3/Phonebook_backend/index.js
@@ -25,15 +25,18 @@ const unknownEndpoint = (request, response) => {
 }
  
   //Error handling middleware
-const errorHandler = (error, request, response, next) => {
+  const errorHandler = (error, request, response, next) => {
     console.error(error.message)
   
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
-    } 
+  
+    } else if (error.name === 'ValidationError') {
+      return response.status(400).json({ error: error.message })
+    }
   
     next(error)
-}
+  }
   
 
 let persons = []
@@ -85,7 +88,7 @@ app.get('/api/persons', (request, response) => {
     })
   })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body  = request.body
     if(!body.name || !body.number){
         return response.status(400).json({
@@ -101,18 +104,17 @@ app.post('/api/persons', (request, response) => {
     person.save().then(savedPerson => {
         response.json(savedPerson)
     })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
-  
-    const person = {
-        name: body.name,
-        number: body.number
-    }
+    const { name, number } = request.body
 
-  
-    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    Person.findByIdAndUpdate(
+      request.params.id, 
+      {name,number}, 
+      { new: true, runValidators: true, context: 'query' }
+    )
       .then(updatedPerson => {
         response.json(updatedPerson)
       })
--- Part-3/Phonebook_backend/models/person.js
+++ Part-3/Phonebook_backend/models/person.js
@@ -17,7 +17,10 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-    name: String,
+    name: {
+      type: String,
+      minLength: 3
+    },
     number: String,
   })
   
