f27ef04a5af6d194aec2b5818092c5cc1d42693e
yashsharma127 <yashshar.2323@gmail.com>
2023-07-20 23:15:10 +0530

    minor code changes

--- Part-2/countrydata/src/App.js
+++ Part-2/countrydata/src/App.js
@@ -1,27 +1,21 @@
 import { useState,useEffect } from "react"
 import axios from 'axios'
 
-const COUNTRIES_IN_LOCALSTORAGE = 'countries';
 
 const App = () =>  {
 
   const [countries, setCountries] = useState([])
 
   useEffect(() => {
-    
-        console.log('Fetching from server');
-        axios
-          .get('https://restcountries.com/v3.1/all')
-            .then((response) => {
-              const fetchedCountries = response.data
-              console.log('Fetched countries:', fetchedCountries);
-              setCountries(fetchedCountries)
-              localStorage.setItem(COUNTRIES_IN_LOCALSTORAGE, JSON.stringify(fetchedCountries))
-        })
-    
-}, [])
+    console.log('Fetching from server');
+    axios.get('https://restcountries.com/v3.1/all').then((response) => {
+        const fetchedCountries = response.data;
+        console.log('Fetched countries:', fetchedCountries); 
+        setCountries(fetchedCountries);
+    });
+}, []);
    
-
+console.log('countries',countries)
   return (
     <div >
       <form>
