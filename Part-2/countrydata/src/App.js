b609297f4c70af4192b59ea852f38bb3a4a73fc8
yashsharma127 <yashshar.2323@gmail.com>
2023-07-21 00:12:31 +0530

    final exersice from 2.18 - 2.20

--- Part-2/countrydata/src/App.js
+++ Part-2/countrydata/src/App.js
@@ -1,28 +1,48 @@
-import { useState,useEffect } from "react"
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import Countries from './components/Countries'
+import Filtercountry from './components/Filtercountry'
 
+function App() {
+    const [countries, setCountries] = useState([])
+    const [localStorageKey, setLocalStorageKey] = useState('countriesData')
 
-const App = () =>  {
+    useEffect(() => {
+      //cache
+        if (!!localStorage.getItem(localStorageKey)) {
+            console.log('using country data from Storage')
+            setCountries(JSON.parse(localStorage.getItem(localStorageKey)))
+        } else {
+          //fetching data using api
+            console.log('Fetching from server')
+            axios.get('https://restcountries.com/v3.1/all').then(result => {
+                const fetchedCountries = result.data
 
-  const [countries, setCountries] = useState([])
+                console.log('Fetched countries:', fetchedCountries)
 
-  useEffect(() => {
-    console.log('Fetching from server');
-    axios.get('https://restcountries.com/v3.1/all').then((response) => {
-        const fetchedCountries = response.data;
-        console.log('Fetched countries:', fetchedCountries); 
-        setCountries(fetchedCountries);
-    });
-}, []);
-   
-console.log('countries',countries)
-  return (
-    <div >
-      <form>
-        find countries: <input />
-      </form>
-    </div>
-  );
+                setCountries(fetchedCountries)
+                localStorage.setItem(localStorageKey, JSON.stringify(fetchedCountries))
+            });
+        }
+    }, [localStorageKey])
+
+    const [filterName, setFilterName] = useState('')
+
+    //name input handler
+    const handleFilterName = (event) => setFilterName(event.target.value)
+
+    //country filter handler case-insesitive
+    const countriesFiltered = countries.filter(country => {
+        return country.name.common.toLowerCase().includes(filterName.toLowerCase())
+    })
+
+    return (
+        <>
+            <Filtercountry filterName={filterName} onChange={handleFilterName} />
+            <div>Filter Name: {filterName}</div>
+            <Countries countries={countriesFiltered} />
+        </>
+    )
 }
 
 export default App;
---
+++ Part-2/countrydata/src/components/Countries.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Country from './Country/Country';
+import CountryInfo from './Country/CountryInfo';
+
+const Countries = ({ countries }) => {
+    if (countries.length > 10) {
+        return <div>Too many matches, specify more.</div>;
+    } 
+    else if (countries.length === 1) {
+        return <CountryInfo country={countries[0]} />;
+    }
+   
+    return (
+        <div>{countries.length <= 10 && countries.map((country) => 
+            <Country country={country} key={country.name.common} />)
+        }</div>
+    );
+};
+
+export default Countries;
